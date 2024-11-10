8de360b1cc56767abbec2887f35e15cd9057051b
Alex <yuriev200@gmail.com>
2024-11-10 17:56:31 +0200

    static members, singleton pattern

--- classes/static.php
+++ classes/static.php
@@ -1,5 +1,36 @@
 <?php
 
-class MathUtils {
+
+class MathUtils
+{
     public static float $pi = 3.14;
+
+    public static function square(float $num): float
+    {
+        return $num * $num;
+    }
 }
+
+var_dump(
+    MathUtils::$pi,
+    MathUtils::square(4)
+);
+
+// Expensive resources - singleton pattern
+
+class Connection
+{
+    private static $instance = null;
+
+    private function __construct() {}
+
+    public static function singleton()
+    {
+        if (static::$instance === null) {
+            return new static();
+        }
+        return static::$instance;
+    }
+}
+
+$connection = Connection::singleton();
